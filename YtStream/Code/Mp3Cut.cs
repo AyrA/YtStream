52e13b1d443eb3aa4fc273ad6e4ba209c76d5b78
Kevin Gut <kevin.gut@bitm.sg>
2022-02-08 22:50:14 +0100

    Change MP3 cutter to act as the duplication for caching too.

--- YtStream/Code/Mp3Cut.cs
+++ YtStream/Code/Mp3Cut.cs
@@ -7,17 +7,57 @@ using YtStream.MP3;
 
 namespace YtStream
 {
+    /// <summary>
+    /// Cuts and filters MP3 data
+    /// </summary>
     public static class Mp3Cut
     {
-        public static void CutMp3(IEnumerable<TimeRange> Ranges, Stream Source, Stream Target)
+        /// <summary>
+        /// Cuts an MP3 file according to ranges.
+        /// Also removes any non-audio bytes.
+        /// </summary>
+        /// <param name="Ranges">
+        /// Rages to cut.
+        /// Can be null or an empty enumeration to filter for audio only
+        /// </param>
+        /// <param name="Source">
+        /// Source stream with uncut, unfiltered MP3 data
+        /// </param>
+        /// <param name="Target">
+        /// Target stream for filtered and cut MP3 data
+        /// </param>
+        /// <param name="UncutOutput">
+        /// Target stream for filtered only data.
+        /// Can be null if not in use.
+        /// </param>
+        public static void CutMp3(IEnumerable<TimeRange> Ranges, Stream Source, Stream Target, Stream UncutOutput = null)
         {
-            Task.WaitAll(CutMp3Async(Ranges, Source, Target));
+            Task.WaitAll(CutMp3Async(Ranges, Source, Target, UncutOutput));
         }
 
-        public static async Task CutMp3Async(IEnumerable<TimeRange> Ranges, Stream Source, Stream Target)
+        /// <summary>
+        /// Cuts an MP3 file according to ranges.
+        /// Also removes any non-audio bytes.
+        /// </summary>
+        /// <param name="Ranges">
+        /// Rages to cut.
+        /// Can be null or an empty enumeration to filter for audio only
+        /// </param>
+        /// <param name="Source">
+        /// Source stream with uncut, unfiltered MP3 data
+        /// </param>
+        /// <param name="Target">
+        /// Target stream for filtered and cut MP3 data
+        /// </param>
+        /// <param name="UncutOutput">
+        /// Target stream for filtered only data.
+        /// Can be null if not in use.
+        /// </param>
+        public static async Task CutMp3Async(IEnumerable<TimeRange> Ranges, Stream Source, Stream Target, Stream UncutOutput = null)
         {
             TimeRange[] R;
             double TimeMS = 0.0;
+            //If null, the caller wants to filter invalid data only
             if (Ranges == null)
             {
                 R = new TimeRange[0];
@@ -25,6 +65,10 @@ namespace YtStream
             else
             {
                 R = Ranges.Where(m => m.IsValid).OrderBy(m => m.Start).ToArray();
+            }
+            if (UncutOutput != null && !UncutOutput.CanWrite)
+            {
+                throw new ArgumentException($"If supplied, {nameof(UncutOutput)} must be writable");
             }
 
             if (Target == null)
@@ -88,6 +132,12 @@ namespace YtStream
                         await Target.WriteAsync(Header, 0, Header.Length);
                         await Target.WriteAsync(Audio, 0, Audio.Length);
                     }
+                    //Always output to filter only stream
+                    if (UncutOutput != null)
+                    {
+                        await UncutOutput.WriteAsync(Header, 0, Header.Length);
+                        await UncutOutput.WriteAsync(Audio, 0, Audio.Length);
+                    }
                     TimeMS += Parsed.AudioLengthMS;
                     //Read next header
                     if (!await GuaranteedRead(Header, 0, Header.Length, Source))
@@ -99,6 +149,15 @@ namespace YtStream
             }
         }
 
+        /// <summary>
+        /// Reads exactly as many bytes as requested.
+        /// Useful for streams that may fill slower than we try to read from them
+        /// </summary>
+        /// <param name="buffer">Buffer to fill</param>
+        /// <param name="offset">Offset to start filling from</param>
+        /// <param name="count">Number of bytes to read</param>
+        /// <param name="source">Stream to read from</param>
+        /// <returns>true if sucessfully read, false if EOF</returns>
         private async static Task<bool> GuaranteedRead(byte[] buffer, int offset, int count, Stream source)
         {
             int total = 0;
