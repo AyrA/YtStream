4add12f4fe27dbde34e32319d0cf2ad57ce279d9
Kevin Gut <kevin.gut@bitm.sg>
2022-02-17 23:26:52 +0100

    Rename action

---
+++ YtStream/Code/MP3/MP3.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YtStream.MP3
+{
+    public static class MP3
+    {
+        public static MP3Header GetInfo(string Filename)
+        {
+            using(var FS = File.OpenRead(Filename))
+            {
+                return GetInfo(FS);
+            }
+        }
+
+        public static MP3Header GetInfo(Stream Source)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
