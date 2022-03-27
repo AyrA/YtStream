d0b4f4d68f395e0e9a3fe912ca8dd4f3798ae110
Kevin Gut <kevin.gut@bitm.sg>
2022-03-27 20:20:11 +0200

    Yt API for playlists

---
+++ YtStream/Code/YT/YtApi.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace YtStream.YT
+{
+    public class YtApi
+    {
+        private const string ApiBase = "https://youtube.googleapis.com/youtube/v3/";
+
+        private readonly string apiKey;
+
+        public YtApi(string ApiKey)
+        {
+            if (string.IsNullOrWhiteSpace(ApiKey))
+            {
+                throw new ArgumentException($"'{nameof(ApiKey)}' cannot be null or whitespace.", nameof(ApiKey));
+            }
+            apiKey = ApiKey;
+        }
+
+        public async Task<YtPlItem[]> GetPlaylistInfo(string PlaylistId)
+        {
+            if (!Tools.IsYoutubePlaylist(PlaylistId))
+            {
+                throw new ArgumentException("Playlist id is invalid");
+            }
+            var URL = BuildUrl("playlistItems", new
+            {
+                part = "snippet",
+                maxResults = 20,
+                playlistId = PlaylistId
+            });
+            var Ret = new List<YtPlItem>();
+            YtPlResult Result;
+            do
+            {
+                try
+                {
+                    Result = (await GetJson(URL)).FromJson<YtPlResult>(true, true);
+                }
+                catch (Exception ex)
+                {
+                    return null;
+                }
+                if (Result == null || Result.Items == null)
+                {
+                    return null;
+                }
+                Ret.AddRange(Result.Items);
+                URL = BuildUrl("playlistItems", new
+                {
+                    part = "snippet",
+                    maxResults = 20,
+                    playlistId = PlaylistId,
+                    pageToken = Result.NextPageToken
+                });
+            } while (!string.IsNullOrEmpty(Result.NextPageToken));
+            return Ret.ToArray();
+        }
+
+        private Uri BuildUrl(string Function, object UrlParams)
+        {
+            var Base = ApiBase + Function + "?key=" + Esc(apiKey);
+            string[] Encoded = null;
+            if (UrlParams != null)
+            {
+                Encoded = UrlParams
+                    .GetType()
+                    .GetProperties()
+                    .Select(m => Esc(m.Name) + "=" + Esc(m.GetValue(UrlParams)))
+                    .ToArray();
+            }
+            if (Encoded != null && Encoded.Length > 0)
+            {
+                return new Uri(Base + "&" + string.Join("&", Encoded));
+            }
+            return new Uri(Base);
+        }
+
+        private static async Task<string> GetJson(Uri Url)
+        {
+            var Req = WebRequest.CreateHttp(Url);
+            Req.Accept = "application/json";
+            Req.UserAgent = "YtStream +https://github.com/AyrA/YtStream";
+            Req.AutomaticDecompression = DecompressionMethods.All;
+            try
+            {
+                using (var Res = (HttpWebResponse)await Req.GetResponseAsync())
+                {
+                    using (var Body = Res.GetResponseStream())
+                    {
+                        return await Tools.ReadStringAsync(Body);
+                    }
+                }
+
+            }
+            catch (WebException ex)
+            {
+                using (var Res = ex.Response)
+                {
+                    using (var Body = Res.GetResponseStream())
+                    {
+                        return await Tools.ReadStringAsync(Body);
+                    }
+                }
+            }
+        }
+
+        private static string Esc(object o)
+        {
+            if (o == null)
+            {
+                return string.Empty;
+            }
+            return Uri.EscapeDataString(o.ToString());
+        }
+    }
+}
---
+++ YtStream/Code/YT/YtPlResult.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace YtStream.YT
+{
+    public class YtPlResult
+    {
+        public string NextPageToken { get; set; }
+        public YtPlItem[] Items { get; set; }
+
+        public YtPageInfo PageInfo { get; set; }
+    }
+
+    public class YtPageInfo
+    {
+        public int TotalResults { get; set; }
+
+        public int ResultsPerPage { get; set; }
+    }
+
+    public class YtPlItem
+    {
+        public YtPlSnippet Snippet { get; set; }
+    }
+
+    public class YtPlSnippet
+    {
+        public DateTime PublishedAt { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public Dictionary<string, YtThumbnail> Thumbnails { get; set; }
+
+        public int Position { get; set; }
+
+        public YtPlVideo ResourceId { get; set; }
+
+        public string VideoOwnerChannelTitle { get; set; }
+
+        public string VideoOwnerChannelId { get; set; }
+    }
+
+    public class YtPlVideo
+    {
+        public string VideoId { get; set; }
+    }
+
+    public class YtThumbnail
+    {
+        public string Url { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+    }
+}
