9a42bc3b06ec9bf7100f46f8c235a9a9aae3e011
Kevin Gut <kevin.gut@bitm.sg>
2022-05-08 00:22:51 +0200

    Remove unnecessary comment

--- YtStream/Code/Converter.cs
+++ YtStream/Code/Converter.cs
@@ -336,16 +336,3 @@ namespace YtStream
         }
     }
 }
-/*
- ffmpeg version 2021-01-20-git-2021dbe1d6-full_build-www.gyan.dev Copyright (c) 2000-2021 the FFmpeg developers
-built with gcc 10.2.0 (Rev6, Built by MSYS2 project)
-configuration: --enable-gpl --enable-version3 --enable-static --disable-w32threads --disable-autodetect --enable-fontconfig --enable-iconv --enable-gnutls --enable-libxml2 --enable-gmp --enable-lzma --enable-libsnappy --enable-zlib --enable-libsrt --enable-libssh --enable-libzmq --enable-avisynth --enable-libbluray --enable-libcaca --enable-sdl2 --enable-libdav1d --enable-libzvbi --enable-librav1e --enable-libsvtav1 --enable-libwebp --enable-libx264 --enable-libx265 --enable-libxvid --enable-libaom --enable-libopenjpeg --enable-libvpx --enable-libass --enable-frei0r --enable-libfreetype --enable-libfribidi --enable-libvidstab --enable-libvmaf --enable-libzimg --enable-amf --enable-cuda-llvm --enable-cuvid --enable-ffnvcodec --enable-nvdec --enable-nvenc --enable-d3d11va --enable-dxva2 --enable-libmfx --enable-libglslang --enable-vulkan --enable-opencl --enable-libcdio --enable-libgme --enable-libmodplug --enable-libopenmpt --enable-libopencore-amrwb --enable-libmp3lame --enable-libshine --enable-libtheora --enable-libtwolame --enable-libvo-amrwbenc --enable-libilbc --enable-libgsm --enable-libopencore-amrnb --enable-libopus --enable-libspeex --enable-libvorbis --enable-ladspa --enable-libbs2b --enable-libflite --enable-libmysofa --enable-librubberband --enable-libsoxr --enable-chromaprint
-libavutil      56. 63.101 / 56. 63.101
-libavcodec     58.117.101 / 58.117.101
-libavformat    58. 65.101 / 58. 65.101
-libavdevice    58. 11.103 / 58. 11.103
-libavfilter     7. 96.100 /  7. 96.100
-libswscale      5.  8.100 /  5.  8.100
-libswresample   3.  8.100 /  3.  8.100
-libpostproc    55.  8.100 / 55.  8.100
-*/
